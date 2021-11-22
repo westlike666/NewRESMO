b9b0af7274e4cfaa6606655fc4ce69bb385f8533
Ruoxi Wang <38963059+westlike666@users.noreply.github.com>
2021-11-22 13:23:38 -0800

    Update kPD_h.m

--- kPD_h.m
+++ kPD_h.m
@@ -1,3 +1,9 @@
 function kpd=kPD_h(n) %helpfunction, only takes single value
     kpd=4.13e7/(2*pi*n^5)*(0.3054+(3e-5)*(n^2-16));
 end
+
+%0.3054 comes from (2*l+1).*[0:3]
+%n^2-16 comes from the sum from l=4 to l=n-1
+
+% to consider only high Rydberg (l>3), modified into
+% kpd_high=4.13e7/(2*pi*n^5)*(3e-5)*(n^2-16); put in new funtion kPD_high
