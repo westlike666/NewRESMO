1a3d9670ea75bf33a9338cf20bbeeb2951bffbbc
Ruoxi Wang <38963059+westlike666@users.noreply.github.com>
2022-02-02 19:14:50 -0800

    Update start_complete.m

--- 1D/start_complete.m
+++ 1D/start_complete.m
@@ -4,9 +4,9 @@ clc
 N=100% number of shells 
 k=round(0.1*N)+1 %k=round(0.1*N)+1; % k is off number of shells between NO^** and NO^+. set to approximately 10% of total shells 
 n0=50;
-dp=0.5;  %   peak density in um^-3 or 1e12 cm^-3
-gamma1=1e-5;
-gamma2=1e-5;
+dp=0.1;  %   peak density in um^-3 or 1e12 cm^-3
+gamma1=1;
+gamma2=1;
 
 save=false
 
@@ -67,7 +67,8 @@ end
 %%
  t=t/1000;
  Vs=2*pi/3*d_phi*(diff(Rs.^3,1,2)); 
- 
+% Vs=1;
+
  tot_ions=sum(sum(N_ions.*Vs,2),3);
  tot_nDens=sum(sum(NDens.*Vs(1,:,:),2),3);
  tot_highs=sum(sum(N_highs.*Vs/8,2),3);  % 1/2 rs yields 1/8 Vs
@@ -160,7 +161,7 @@ for i=[1:length(tspan)]
     set(h, 'EdgeColor', 'none')
     cmap=colormap;
     set(gca,'Color',cmap(1,:))
-%    caxis([0 cmax])
+    %caxis([0 cmax])
     ylabel('relative density')
     axis(15000*[-1 1 -1 1]) 
     title('NO^{**} and NO^+')    
