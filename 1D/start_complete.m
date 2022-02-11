71dcd476fc61473e847738336b590ed5cbd69ab6
Ruoxi Wang <38963059+westlike666@users.noreply.github.com>
2022-02-10 19:32:33 -0800

    Update start_complete.m

--- 1D/start_complete.m
+++ 1D/start_complete.m
@@ -1,19 +1,53 @@
+% This code can produce the 2D results of the shell model:
+% After setting up the parameters, hit Run and wait  
+
 close all
 clear
 clc
-N=100% number of shells 
-k=round(0.1*N)+1 %k=round(0.1*N)+1; % k is off number of shells between NO^** and NO^+. set to approximately 10% of total shells 
-n0=50;
+
+%% Adjustable parmeters to play with
+
+% First choose the purpose of running.  'soc' or 'geo'
+% There is a trade-off of computation cost between searching SOC conditions
+% and showing 2D bifurcation images 
+
+%purpose='soc';
+purpose='geo'
+
+if purpose=='soc'
+    all_pqn=true;  M=1;
+    % all_pqn decides wether to consder all principal quanutum number within [30,80] 
+    % M is the number of azimuth angles in the ellipse spreading from 0 to 2*pi: M=1 produce a 1D result
+    % This configs means it only run the 1D model once (M=1), but will populate all the pqn from 30 to 80 during the calculation 
+    % Use this set-up to quickly search the conditions of self-organized-critical without genrating bifurcation images.  
+elseif purpose=='geo'
+     all_pqn=false; M=50
+    % This configs will run the model 50 times at 50 azimuth angles to construct 2D images
+    % Considering all pricipal quantum number in [30,80] is not recommnded due to computation time  
+    % Use this set-up to genrate bifurcation images.  
+end 
+
+N=100 % number of shells 
+
+
+n0=50; % initial principal quanutm number  
 dp=0.1;  %   peak density in um^-3 or 1e12 cm^-3
-gamma1=0.01;
-gamma2=0;
+gamma1=0.01; % This is the magnitude of charge transfer rate: k_CT=gamma1.*dr.*(a0*n.^2); The dimesion is arbitary
 
-save=false
+save=false % whether so save images as movie
 
-cmax=0.012
-t_final=1e5;  % times are in ns 
-dt=1e3;
-tspan=[0:dt:t_final];
+
+
+
+%% parameters that are suggested to be fixed 
+gamma2=0; % For now ignoring the charge transfer with high Rydberg: NO^+ e + NO** -> 2NO**
+
+cmax=0.012; % color axis limit 
+t_final=1e5;  % evolution time in ns 
+dt=1e3; % steps of time to demonstrate
+tspan=[0:dt:t_final]; 
+
+k=round(0.1*N)+1; %k=round(0.1*N)+1; % k is off number of shells between NO^** and NO^+. set to approximately 10% of total shells 
 
 % r(theta)= a*b/sqrt(b^2 cos)
 
@@ -22,12 +56,10 @@ tspan=[0:dt:t_final];
 %     sigma_z=0.70*1000;  % Gaussian width convert from mm to um defualt 0.42
 
 
-a=1000; % convert mm to nm
-b=0.5*1000;
+a=1000; % width of ellipse, convert mm to nm
+b=0.5*1000; % height of ellipse. 
 
-env=5;  % sigma environment
-
-M=50;
+env=5;  % sigma environment of density distribution 
 
 
 %filename=['2D/'  'den=' num2str(dp), ' n0=' num2str(n0) ' a=', num2str(a), ' b=', num2str(b),  ' gamma1=' num2str(gamma1), ' gamma2=' num2str(gamma2) ' k_pd=' num2str(k_pd)];
@@ -50,7 +82,7 @@ NDens=zeros(length(tspan),N,M);
 for m=[1:M]
     sigma=R(m);
     phi=Phi(m);
-    [t,rs, edens, n_ions, n_highs, nDens, Ts, Tn, r0, tau, ns]=complete1D(N,k,dp,n0,sigma,env,tspan,gamma1,gamma2, phi, d_phi);
+    [t,rs, edens, n_ions, n_highs, nDens, Ts, Tn, r0, tau, ns]=complete1D(N,k,dp,n0,all_pqn,sigma,env,tspan,gamma1,gamma2, phi, d_phi);
     [x,y] = pol2cart(Phi(m),rs(:,1:end-1));
     X(:,:,m)=x;
     Y(:,:,m)=y;
@@ -65,9 +97,13 @@ for m=[1:M]
     
 end 
 %%
- t=t/1000;
- Vs=2*pi/3*d_phi*(diff(Rs.^3,1,2)); 
-% Vs=1;
+ t=t/1000; % converting time from ns to um
+
+ if purpose=='soc'
+     Vs=2*pi/3*d_phi*(diff(Rs.^3,1,2)); 
+ elseif purpose=='geo'
+     Vs=1; 
+ end
 
  tot_ions=sum(sum(N_ions.*Vs,2),3);
  tot_nDens=sum(sum(NDens.*Vs(1,:,:),2),3);
@@ -93,91 +129,92 @@ xlabel('us')
 figure()
 subplot(2,1,1)
 plot(t,rs)
-ylabel('r')
+ylabel('r/um')
 xlabel('us')
 
 subplot(2,1,2)
 plot(t,Ts)
-ylabel('T_e')
+ylabel('T_e/K')
 xlabel('us')
 
-if save
-     v=VideoWriter([filename '.avi']);
-     open(v);
-end
+% if save
+%      v=VideoWriter([filename '.avi']);
+%      open(v);
+% end
 
-figure()
-for i=[1:length(tspan)]
-    T=tspan(i);
-    xx=reshape(X(i,:,:),[N,M]);
-    yy=reshape(Y(i,:,:),[N,M]);
-    x0=reshape(X(1,:,:),[N,M]);
-    y0=reshape(Y(1,:,:),[N,M]);
-    
-    nn_ions=reshape(N_ions(i,:,:),N,M);
-    nn_highs=reshape(N_highs(i,:,:),N,M);
-    nnDens=reshape(NDens(i,:,:),N,M);
-    
-%     nn_ions=reshape(N_ions(i,:,:).*Vs(i,:,:),N,M);
-%     nn_highs=reshape(N_highs(i,:,:).*Vs(i,:,:)/8,N,M);
-%     nnDens=reshape(NDens(i,:,:).*Vs(1,:,:),N,M);
-    
-    subplot(2,2,1)
-%    colormap winter
-    h=pcolor(x0,y0,nnDens);
-    set(h, 'EdgeColor', 'none')
-    cmap=colormap;
-    set(gca,'Color',cmap(1,:))
- %   caxis([0 cmax])
-    title('NO^* Rydberg')
-    %axis(15000*[-1 1 -1 1])
-    
-    subplot(2,2,2)
-%    colormap turbo
-    h=pcolor(xx,yy,nn_ions);
-    set(h, 'EdgeColor', 'none')
-    cmap=colormap;
-    set(gca,'Color',cmap(1,:))
-%    caxis([0 cmax])
-    axis(15000*[-1 1 -1 1])
-    title('NO^+ ions')
-    
-    subplot(2,2,3)
-%    colormap parula
-    h=pcolor(0.5*xx,0.5*yy,nn_highs);
-    set(h, 'EdgeColor', 'none')
-    cmap=colormap;
-    set(gca,'Color',cmap(1,:))
-%    caxis([0 cmax])
-    caxis
-    ylabel('relative density')
-    axis(10000*[-1 1 -1 1]) 
-    title('NO^{**} long-lived Rydberg')
-    xlabel('r/ \sigma_0')
-    
-    subplot(2,2,4)
-%    colormap parula
-    h=pcolor(xx,yy,nn_ions+[nn_highs(k+1:N,:); zeros(k,M)]);   % plot the overlapping region 
-    set(h, 'EdgeColor', 'none')
-    cmap=colormap;
-    set(gca,'Color',cmap(1,:))
-    %caxis([0 cmax])
-    ylabel('relative density')
-    axis(15000*[-1 1 -1 1]) 
-    title('NO^{**} and NO^+')    
-    
-    xlabel('r/ \sigma_0')
-    
+if purpose=='geo'
+    figure()
+    for i=[1:length(tspan)]
+        T=tspan(i);
+        xx=reshape(X(i,:,:),[N,M]);
+        yy=reshape(Y(i,:,:),[N,M]);
+        x0=reshape(X(1,:,:),[N,M]);
+        y0=reshape(Y(1,:,:),[N,M]);
+        
+        nn_ions=reshape(N_ions(i,:,:),N,M);
+        nn_highs=reshape(N_highs(i,:,:),N,M);
+        nnDens=reshape(NDens(i,:,:),N,M);
+        
+    %     nn_ions=reshape(N_ions(i,:,:).*Vs(i,:,:),N,M);
+    %     nn_highs=reshape(N_highs(i,:,:).*Vs(i,:,:)/8,N,M);
+    %     nnDens=reshape(NDens(i,:,:).*Vs(1,:,:),N,M);
+        
+        subplot(2,2,1)
+    %    colormap winter
+        h=pcolor(x0,y0,nnDens);
+        set(h, 'EdgeColor', 'none')
+        cmap=colormap;
+        set(gca,'Color',cmap(1,:))
+     %   caxis([0 cmax])
+        title('NO^* Rydberg')
+        %axis(15000*[-1 1 -1 1])
+        
+        subplot(2,2,2)
+    %    colormap turbo
+        h=pcolor(xx,yy,nn_ions);
+        set(h, 'EdgeColor', 'none')
+        cmap=colormap;
+        set(gca,'Color',cmap(1,:))
+    %    caxis([0 cmax])
+        axis(15000*[-1 1 -1 1])
+        title('NO^+ ions')
+        
+        subplot(2,2,3)
+    %    colormap parula
+        h=pcolor(0.5*xx,0.5*yy,nn_highs);
+        set(h, 'EdgeColor', 'none')
+        cmap=colormap;
+        set(gca,'Color',cmap(1,:))
+    %    caxis([0 cmax])
+        caxis
+        ylabel('relative density')
+        axis(10000*[-1 1 -1 1]) 
+        title('NO^{**} long-lived Rydberg')
+        xlabel('r/um')
+        
+        subplot(2,2,4)
+    %    colormap parula
+        h=pcolor(xx,yy,nn_ions+[nn_highs(k+1:N,:); zeros(k,M)]);   % plot the overlapping region 
+        set(h, 'EdgeColor', 'none')
+        cmap=colormap;
+        set(gca,'Color',cmap(1,:))
+        %caxis([0 cmax])
+        ylabel('relative density')
+        axis(15000*[-1 1 -1 1]) 
+        title('NO^{**} and NO^+')        
+        xlabel('r/um')
+        
+        if save
+            frame=getframe(gcf);
+            writeVideo(v,frame); 
+        else
+            pause(0.1)
+        end
+    end    
+
     if save
-        frame=getframe(gcf);
-        writeVideo(v,frame); 
-    else
-        pause(0.1)
+    close(v)
     end
-end    
-
-if save
-close(v)
 end
 
 
